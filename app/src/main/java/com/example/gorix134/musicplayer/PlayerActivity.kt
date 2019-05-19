80a23d5dbe3db1bef53da82ac4491e51481c74c5
Gor1x <ea-post@mail.ru>
2019-05-19 23:13:50 +0500

    added main screen and button

--- app/src/main/java/com/example/gorix134/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/gorix134/musicplayer/PlayerActivity.kt
@@ -3,11 +3,10 @@ package com.example.gorix134.musicplayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_player)
     }
 }
