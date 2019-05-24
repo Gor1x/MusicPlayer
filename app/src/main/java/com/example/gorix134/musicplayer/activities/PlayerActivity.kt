8e3e05833760e71f72823e464c47c27f9eb1eb72
Gor1x <ea-post@mail.ru>
2019-05-25 01:55:43 +0500

    Добавил всплывающее снизу окно
    Signed-off-by: Gor1x <ea-post@mail.ru>

--- app/src/main/java/com/example/gorix134/musicplayer/activities/PlayerActivity.kt
+++ app/src/main/java/com/example/gorix134/musicplayer/activities/PlayerActivity.kt
@@ -5,8 +5,13 @@ import PATH_KEY
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.example.gorix134.musicplayer.R
+import kotlinx.android.synthetic.main.activity_player.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -16,6 +21,21 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+        val bottomBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                cardView.animate().scaleX(1F - slideOffset).scaleY(1F - slideOffset).setDuration(0).start()
+            }
+        })
+
+        cardView.setOnClickListener {
+            bottomBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
     }
 
     private fun checkPreferencesForFolder() {
