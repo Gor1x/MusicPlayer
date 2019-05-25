f99cbad2b77a62efe41ad98385c173fabbc66e54
Gor1x <ea-post@mail.ru>
2019-05-26 03:32:58 +0500

    Добавил проигрыватель, но он не работает(

--- app/src/main/java/com/example/gorix134/musicplayer/activities/PlayerActivity.kt
+++ app/src/main/java/com/example/gorix134/musicplayer/activities/PlayerActivity.kt
@@ -2,29 +2,83 @@ package com.example.gorix134.musicplayer.activities
 
 import APP_PREFERENCES
 import PATH_KEY
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.support.constraint.ConstraintLayout
 import android.support.design.widget.BottomSheetBehavior
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
+import com.example.gorix134.musicplayer.PlayerService
 import com.example.gorix134.musicplayer.R
 import kotlinx.android.synthetic.main.bottom_sheet.*
-import kotlinx.android.synthetic.main.player_panel.*
 
 
 class PlayerActivity : AppCompatActivity() {
 
 
+    private var playerServiceBinder: PlayerService.PlayerServiceBinder? = null;
     private var currentPath = "" //Empty if it is a first run
     private lateinit var bottomBehavior: BottomSheetBehavior<ConstraintLayout>;
+    private lateinit var callback: MediaControllerCompat.Callback;
+    private lateinit var serviceConnection: ServiceConnection;
+    private var mediaController: MediaControllerCompat? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
         initBottomSheet();
+
+        callback = object : MediaControllerCompat.Callback() {
+            override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+                if (state == null)
+                    return;
+                val playing = (state.state == PlaybackStateCompat.STATE_PLAYING);
+                statePlayChanged(playing);
+            }
+        }
+
+        serviceConnection = object : ServiceConnection {
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                playerServiceBinder = null;
+                if (mediaController != null) {
+                    mediaController!!.unregisterCallback(callback);
+                    mediaController = null;
+                }
+            }
+
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                playerServiceBinder = service as PlayerService.PlayerServiceBinder;
+                try {
+                    mediaController =
+                        MediaControllerCompat(applicationContext, playerServiceBinder!!.mediaSessionToken);
+                    mediaController!!.registerCallback(callback);
+                    callback.onPlaybackStateChanged(mediaController!!.playbackState);
+                } catch (e: Exception) {
+                    mediaController = null;
+                }
+            }
+
+        }
+
+        bindService(Intent(this, PlayerService::class.java), serviceConnection, Context.BIND_AUTO_CREATE);
+
+        songPanelPlay.setOnClickListener {
+            mediaController?.transportControls?.play();
+        }
+
+        songPanelNext.setOnClickListener {
+            mediaController?.transportControls?.skipToNext();
+        }
+
+        songPanelPause.setOnClickListener {
+            mediaController?.transportControls?.pause();
+        }
     }
 
     private fun initBottomSheet() {
@@ -41,12 +95,14 @@ class PlayerActivity : AppCompatActivity() {
             }
         })
 
-        if (playerPanel == null) {
-            Log.d("BUGS", "it is null");
-        }
-
         playerPanel.addOnLayoutChangeListener { view, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
             bottomBehavior.peekHeight = bottom;
+        }
+        songPanelNext.addOnLayoutChangeListener { view, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+            val lp = songPanelPlay.layoutParams as ConstraintLayout.LayoutParams
+            lp.width = view.width;
+            lp.height = view.height;
+            songPanelPlay.layoutParams = lp;
         }
     }
 
@@ -63,6 +119,22 @@ class PlayerActivity : AppCompatActivity() {
         if (currentPath == "") {
             val intent = Intent(applicationContext, ButtonActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun statePlayChanged(playing: Boolean) {
+        if (playing) {
+            songPanelPlay.visibility = View.INVISIBLE;
+            songPanelPlay.isEnabled = false;
+
+            songPanelPause.visibility = View.VISIBLE;
+            songPanelPause.isEnabled = true;
+        } else {
+            songPanelPlay.visibility = View.VISIBLE;
+            songPanelPlay.isEnabled = true;
+
+            songPanelPause.visibility = View.INVISIBLE;
+            songPanelPause.isEnabled = false;
         }
     }
 
